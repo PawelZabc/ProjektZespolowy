3e37332a7b14ebbb64a04157fa0fe3d8d5cb66c6
Paweł Żabczyński <p_k_zab@wp.pl>
2025-11-12 13:31:41 +0100

    add pushback to cube and cylinder collider

--- client/Collider.go
+++ client/Collider.go
@@ -17,6 +17,7 @@ type Collider interface {
 	GetPosition() rl.Vector3
 	SetPosition(rl.Vector3)
 	AddPosition(rl.Vector3)
+	PushbackFrom(Collider)
 }
 
 type CylinderCollider struct {
@@ -94,4 +95,156 @@ func (c *CubeCollider) SetPosition(vec rl.Vector3) {
 
 func (c *CubeCollider) AddPosition(vec rl.Vector3) {
 	c.Position = rl.Vector3Add(c.Position, vec)
+}
+
+func (c *CubeCollider) PushbackFrom(c2 Collider) {
+	if cube, ok := c2.(*CubeCollider); ok {
+		c.PushbackFromCube(cube)
+	} else if cylinder, ok := c2.(*CylinderCollider); ok {
+		c.PushbackFromCylinder(cylinder)
+	}
+
+}
+
+func (c *CylinderCollider) PushbackFrom(c2 Collider) {
+	if cylinder, ok := c2.(*CylinderCollider); ok {
+		c.PushbackFromCylinder(cylinder)
+
+		// println(distanceXZ, distanceY1, distanceY2)
+
+	} else if cube, ok := c2.(*CubeCollider); ok {
+		c.PushbackFromCube(cube)
+	}
+
+}
+
+func (c *CubeCollider) PushbackFromCube(cube *CubeCollider) {
+	x1 := c.Position.X - (cube.Position.X + cube.SizeX)
+	x2 := cube.Position.X - (c.Position.X + c.SizeX)
+	y1 := c.Position.Y - (cube.Position.Y + cube.SizeY)
+	y2 := cube.Position.Y - (c.Position.Y + c.SizeY)
+	z1 := c.Position.Z - (cube.Position.Z + cube.SizeZ)
+	z2 := cube.Position.Z - (c.Position.Z + c.SizeZ)
+	// println(x1, x2, y1, y2, z1, z2)
+	if x1 <= 0 && x2 <= 0 && y1 <= 0 && y2 <= 0 && z1 <= 0 && z2 <= 0 {
+		xmax := math.Max(x1, x2)
+		ymax := math.Max(y1, y2)
+		zmax := math.Max(z1, z2)
+
+		if xmax > zmax {
+			if xmax > ymax {
+				if x1 > x2 {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(-x1, 0, 0))
+					print("right ")
+				} else {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(x2, 0, 0))
+					print("left ")
+				}
+			} else {
+				if y1 > y2 {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, -y1, 0))
+					print("up ")
+				} else {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, y2, 0))
+					print("down ")
+				}
+			}
+		} else {
+			if zmax > ymax {
+				if z1 > z2 {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, 0, -z1))
+					print("foward ")
+				} else {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, 0, z2))
+					print("backward ")
+				}
+			} else {
+				if y1 > y2 {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, -y1, 0))
+					print("up ")
+				} else {
+					c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, y2, 0))
+					print("down ")
+				}
+
+			}
+
+		}
+	}
+}
+
+func (c *CubeCollider) PushbackFromCylinder(cylinder *CylinderCollider) {
+	diffrence := rl.Vector2Subtract(
+		rl.Vector2{X: math.Min(c.Position.X+c.SizeX, math.Max(c.Position.X, cylinder.Position.X)),
+			Y: math.Min(c.Position.Z+c.SizeZ, math.Max(c.Position.Z, cylinder.Position.Z))},
+		rl.Vector2{X: cylinder.Position.X, Y: cylinder.Position.Z})
+	distanceXZ := rl.Vector2Length(diffrence) - (cylinder.Radius)
+	distanceY1 := c.Position.Y - (cylinder.Position.Y + cylinder.Height)
+	distanceY2 := cylinder.Position.Y - (c.Position.Y + c.SizeY)
+
+	if distanceXZ <= 0 && distanceY1 <= 0 && distanceY2 <= 0 {
+		if distanceXZ > distanceY1 && distanceXZ > distanceY2 {
+			print("side ")
+			forceXZ := rl.Vector2Scale(rl.Vector2Normalize(diffrence), -distanceXZ)
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(forceXZ.X, 0, forceXZ.Y))
+
+		} else if distanceY1 > distanceY2 {
+			print("up ")
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, -distanceY1, 0))
+		} else {
+			print("down ")
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, distanceY2, 0))
+		}
+	}
+
+}
+
+func (c *CylinderCollider) PushbackFromCube(cube *CubeCollider) {
+	diffrence := rl.Vector2Subtract(rl.Vector2{X: c.Position.X, Y: c.Position.Z},
+		rl.Vector2{X: math.Min(cube.Position.X+cube.SizeX, math.Max(cube.Position.X, c.Position.X)),
+			Y: math.Min(cube.Position.Z+cube.SizeZ, math.Max(cube.Position.Z, c.Position.Z))})
+	distanceXZ := rl.Vector2Length(diffrence) - (c.Radius)
+	distanceY1 := c.Position.Y - (cube.Position.Y + cube.SizeY)
+	distanceY2 := cube.Position.Y - (c.Position.Y + c.Height)
+
+	if distanceXZ <= 0 && distanceY1 <= 0 && distanceY2 <= 0 {
+		if distanceXZ > distanceY1 && distanceXZ > distanceY2 {
+			print("side ")
+			forceXZ := rl.Vector2Scale(rl.Vector2Normalize(diffrence), -distanceXZ)
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(forceXZ.X, 0, forceXZ.Y))
+
+		} else if distanceY1 > distanceY2 {
+			print("up ")
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, -distanceY1, 0))
+		} else {
+			print("down ")
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, distanceY2, 0))
+		}
+	}
+
+}
+
+func (c *CylinderCollider) PushbackFromCylinder(cylinder *CylinderCollider) {
+	diffrence := rl.Vector2Subtract(rl.Vector2{X: c.Position.X, Y: c.Position.Z},
+		rl.Vector2{X: cylinder.Position.X, Y: cylinder.Position.Z})
+	distanceXZ := rl.Vector2Length(diffrence) - (c.Radius + cylinder.Radius)
+	distanceY1 := c.Position.Y - (cylinder.Position.Y + cylinder.Height)
+	distanceY2 := cylinder.Position.Y - (c.Position.Y + c.Height)
+
+	if distanceXZ <= 0 && distanceY1 <= 0 && distanceY2 <= 0 {
+		if distanceXZ > distanceY1 && distanceXZ > distanceY2 {
+			print("side ")
+			forceXZ := rl.Vector2Scale(rl.Vector2Normalize(diffrence), -distanceXZ)
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(forceXZ.X, 0, forceXZ.Y))
+
+		} else if distanceY1 > distanceY2 {
+			print("up ")
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, -distanceY1, 0))
+		} else {
+			print("down ")
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, distanceY2, 0))
+		}
+
+	}
+
 }
