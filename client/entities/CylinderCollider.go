c21b7b32f08d13fb9b1d8ed4a5ff5c560e0caac1
Paweł Żabczyński <p_k_zab@wp.pl>
2025-11-17 16:05:35 +0100

    add pushback direction return and split Collider.go into two files

---
+++ client/entities/CylinderCollider.go
@@ -0,0 +1,112 @@
+package entities
+
+import (
+	types "github.com/PawelZabc/ProjektZespolowy/client/_types"
+	math "github.com/chewxy/math32"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type CylinderCollider struct {
+	Position rl.Vector3
+	Radius   float32
+	Height   float32
+}
+
+func (c CylinderCollider) CollidesWith(c2 Collider) bool {
+	if cylinder, ok := c2.(*CylinderCollider); ok {
+		if rl.Vector2Distance(rl.Vector2{X: c.Position.X, Y: c.Position.Z},
+			rl.Vector2{X: cylinder.Position.X, Y: cylinder.Position.Z}) < (c.Radius+cylinder.Radius) &&
+			c.Position.Y <= cylinder.Position.Y+cylinder.Height && c.Position.Y+c.Height >= cylinder.Position.Y {
+			return true
+		}
+
+	} else if cube, ok := c2.(*CubeCollider); ok {
+		if rl.Vector2Distance(rl.Vector2{X: math.Min(cube.Position.X+cube.SizeX, math.Max(cube.Position.X, c.Position.X)),
+			Y: math.Min(cube.Position.Z+cube.SizeZ, math.Max(cube.Position.Z, c.Position.Z))},
+			rl.Vector2{X: c.Position.X, Y: c.Position.Z}) <= c.Radius &&
+			cube.Position.Y <= c.Position.Y+c.Height && cube.Position.Y+cube.SizeY >= c.Position.Y {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (c CylinderCollider) GetPosition() rl.Vector3 {
+	return c.Position
+}
+
+func (c *CylinderCollider) SetPosition(vec rl.Vector3) {
+	c.Position = vec
+}
+
+func (c *CylinderCollider) AddPosition(vec rl.Vector3) {
+	c.Position = rl.Vector3Add(c.Position, vec)
+}
+
+func (c *CylinderCollider) PushbackFrom(c2 Collider) types.Direction {
+	if cylinder, ok := c2.(*CylinderCollider); ok {
+		return c.PushbackFromCylinder(cylinder)
+
+	} else if cube, ok := c2.(*CubeCollider); ok {
+		return c.PushbackFromCube(cube)
+	}
+
+	return types.None
+
+}
+
+func (c *CylinderCollider) PushbackFromCube(cube *CubeCollider) types.Direction {
+	diffrence := rl.Vector2Subtract(rl.Vector2{X: c.Position.X, Y: c.Position.Z},
+		rl.Vector2{X: math.Min(cube.Position.X+cube.SizeX, math.Max(cube.Position.X, c.Position.X)),
+			Y: math.Min(cube.Position.Z+cube.SizeZ, math.Max(cube.Position.Z, c.Position.Z))})
+	distanceXZ := rl.Vector2Length(diffrence) - (c.Radius)
+	distanceY1 := c.Position.Y - (cube.Position.Y + cube.SizeY)
+	distanceY2 := cube.Position.Y - (c.Position.Y + c.Height)
+
+	if distanceXZ <= 0 && distanceY1 <= 0 && distanceY2 <= 0 {
+		if distanceXZ > distanceY1 && distanceXZ > distanceY2 {
+			forceXZ := rl.Vector2Scale(rl.Vector2Normalize(diffrence), -distanceXZ)
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(forceXZ.X, 0, forceXZ.Y))
+			return types.XZ
+
+		} else if distanceY1 > distanceY2 {
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, -distanceY1, 0))
+			return -types.Y
+		} else {
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, distanceY2, 0))
+			return types.Y
+		}
+	} else {
+		return types.None
+	}
+
+}
+
+func (c *CylinderCollider) PushbackFromCylinder(cylinder *CylinderCollider) types.Direction {
+	diffrence := rl.Vector2Subtract(rl.Vector2{X: c.Position.X, Y: c.Position.Z},
+		rl.Vector2{X: cylinder.Position.X, Y: cylinder.Position.Z})
+	distanceXZ := rl.Vector2Length(diffrence) - (c.Radius + cylinder.Radius)
+	distanceY1 := c.Position.Y - (cylinder.Position.Y + cylinder.Height)
+	distanceY2 := cylinder.Position.Y - (c.Position.Y + c.Height)
+
+	if distanceXZ <= 0 && distanceY1 <= 0 && distanceY2 <= 0 {
+		if distanceXZ > distanceY1 && distanceXZ > distanceY2 {
+			forceXZ := rl.Vector2Scale(rl.Vector2Normalize(diffrence), -distanceXZ)
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(forceXZ.X, 0, forceXZ.Y))
+			return types.XZ
+
+		} else if distanceY1 > distanceY2 {
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, -distanceY1, 0))
+			return -types.Y
+		} else {
+			c.Position = rl.Vector3Add(c.Position, rl.NewVector3(0, distanceY2, 0))
+			return types.Y
+		}
+
+	} else {
+		return types.None
+	}
+
+}
