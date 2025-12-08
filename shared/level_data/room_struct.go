cf06508e61e8e373cbe9bfd1c4e67a1a11c7eed2
Paweł Żabczyński <p_k_zab@wp.pl>
2025-12-08 18:39:01 +0100

    Add comments and edit Object struct

--- shared/level_data/room_struct.go
+++ shared/level_data/room_struct.go
@@ -6,12 +6,15 @@ import (
 )
 
 type Room struct {
-	Objects      []*Object
-	Id           uint16
-	VisibleRooms []*Room
+	Objects       []*Object //objects and walls in that room
+	SharedObjects []*Object //objects shared with visible rooms
+	Id            uint16
+	VisibleRooms  []*Room          //rooms visible that need to be rendered while in the room
+	RoomChangers  []types.Collider //Colliders determining whenroom is changed
 }
 
 type Object struct {
 	Colliders []types.Collider
-	Model     rl.Model
+	DrawPoint rl.Vector3
+	Model     string
 }
