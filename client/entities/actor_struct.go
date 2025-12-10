f8046ca7d162167e67aaebf4e7db60e0efb940da
Paweł Żabczyński <p_k_zab@wp.pl>
2025-12-10 10:42:31 +0100

    Add function to draw Actors using map

--- client/entities/actor_struct.go
+++ client/entities/actor_struct.go
@@ -23,12 +23,20 @@ func DrawActors(actors []*Actor) {
 	}
 }
 
+func DrawActorsMap[T comparable](actors map[T]*Actor) {
+	for _, actor := range actors {
+		actor.Draw()
+	}
+}
+
 func (a *Actor) Draw() {
 	rl.DrawModelEx(a.Object.Model, rl.Vector3Add(a.Object.DrawPoint, a.Colliders[0].GetPosition()), rl.NewVector3(0, 1, 0), a.Rotation, rl.Vector3One(), a.Object.Color)
 }
 
 func NewActor(collider types.Collider, drawPoint rl.Vector3, rotation float32, modelName string) *Actor {
+	// fmt.Println("Pre Load Actor Model !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 	model, _ := assets.GlobalManager.LoadModel(modelName)
+	// fmt.Println("Load Actor Model Error", err)
 	object := game.Object{Model: model.Data, DrawPoint: drawPoint, Color: rl.White, Colliders: []types.Collider{collider}}
 	actor := Actor{Object: object, Rotation: rotation}
 	return &actor
