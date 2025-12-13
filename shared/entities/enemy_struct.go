f14d9e15ca55c995a36eb8c44100c4161e08cd96
Paweł Żabczyński <p_k_zab@wp.pl>
2025-12-14 00:35:47 +0100

    Add attacking and state to enemy

--- shared/entities/enemy_struct.go
+++ shared/entities/enemy_struct.go
@@ -7,10 +7,67 @@ import (
 )
 
 type Enemy struct {
-	RotationX float32
-	Collider  types.Collider
-	Target    *rl.Vector2
-	Speed     float32
+	RotationX      float32
+	Collider       types.Collider
+	Target         *rl.Vector2
+	Speed          float32
+	State          types.EnemyState
+	AttackTimer    uint8
+	AttackCooldown uint8
+}
+
+func (e *Enemy) Attack(players []*Player, colliders *[]types.Collider) {
+	for _, player := range players {
+		if e.GetDistanceToCollider(player.Collider) < 15 {
+			ray := e.GetRayFromTopToColliderTop(player.Collider)
+			point, length := ray.GetCollisionPoint(player.Collider)
+			if point != nil && length < 10 {
+				direct := true
+				for _, collider := range *colliders {
+					point2, length2 := ray.GetCollisionPoint(collider)
+					if point2 != nil && length2 < length {
+						direct = false
+						break
+					}
+				}
+				if direct {
+					player.Hit(5)
+				}
+			}
+
+		}
+	}
+	e.AttackCooldown = 20
+}
+
+func (e *Enemy) Update(players []*Player, colliders *[]types.Collider) {
+	if e.AttackCooldown > 0 {
+		e.AttackCooldown -= 1
+	}
+	switch e.State {
+	case types.Walking:
+		e.UpdateTarget(players, colliders)
+		e.Move()
+	case types.Attacking:
+		e.AttackTimer -= 1
+		if e.AttackTimer <= 0 {
+			e.Attack(players, colliders)
+			e.State = types.Walking
+		}
+	}
+}
+
+func (e *Enemy) SetState(state types.EnemyState) {
+	if state != e.State {
+		if state == types.Attacking {
+			if e.AttackCooldown == 0 {
+				e.State = types.Attacking
+				e.AttackTimer = 50
+			}
+		} else {
+			e.State = state
+		}
+	}
 }
 
 func (e *Enemy) Move() {
@@ -40,16 +97,7 @@ func (e *Enemy) UpdateTarget(players []*Player, colliders *[]types.Collider) {
 	minId := -1
 	for i, player := range players {
 
-		playerHeight := float32(0)
-		enemyHeight := float32(0)
-		if cylinder, ok := player.Collider.(*CylinderCollider); ok {
-			playerHeight = cylinder.Height
-		}
-		if cylinder2, ok := e.Collider.(*CylinderCollider); ok {
-			enemyHeight = cylinder2.Height
-		}
-		ray := Ray{Origin: rl.Vector3Add(e.Collider.GetPosition(), rl.NewVector3(0, enemyHeight, 0)),
-			Direction: rl.Vector3Normalize(rl.Vector3Subtract(rl.Vector3Add(player.GetPosition(), rl.NewVector3(0, playerHeight, 0)), rl.Vector3Add(e.Collider.GetPosition(), rl.NewVector3(0, enemyHeight, 0))))}
+		ray := e.GetRayFromTopToColliderTop(player.Collider)
 		point, length := ray.GetCollisionPoint(player.Collider)
 
 		difference := ray.GetRotationX() - e.RotationX
@@ -97,4 +145,11 @@ func (e *Enemy) GetDistanceToCollider(collider types.Collider) float32 {
 	target := GetVector2XZ(collider.GetPosition())
 	position := GetVector2XZ(e.Collider.GetPosition())
 	return rl.Vector2Distance(target, position)
+}
+
+func (e *Enemy) GetRayFromTopToColliderTop(collider types.Collider) Ray { //think of a better function name
+	colliderHeight := collider.GetSizeOnAxis(types.DirY)
+	enemyHeight := e.Collider.GetSizeOnAxis(types.DirY)
+	return Ray{Origin: rl.Vector3Add(e.Collider.GetPosition(), rl.NewVector3(0, enemyHeight, 0)),
+		Direction: rl.Vector3Normalize(rl.Vector3Subtract(rl.Vector3Add(collider.GetPosition(), rl.NewVector3(0, colliderHeight, 0)), rl.Vector3Add(e.Collider.GetPosition(), rl.NewVector3(0, enemyHeight, 0))))}
 }
