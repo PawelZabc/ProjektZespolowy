7eade8aaad8fc239abdd421e7f731ebfec3a2b5b
Tomasz Petrykowski <t.petrykowski.069@studms.ug.edu.pl>
2025-12-21 22:40:30 +0100

    Extend server app with update loop and minor fixes

--- internal/server/app.go
+++ internal/server/app.go
@@ -1,14 +1,20 @@
 package server
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/PawelZabc/ProjektZespolowy/internal/config"
 )
 
 type App struct {
-	config  config.ServerConfig
-	network *Network
+	config        config.ServerConfig
+	network       *Network
+	gameState     *GameState
+	physicsEngine *Physics
+	clientManager *ClientManager
+
 	running bool
 }
 
@@ -21,14 +27,18 @@ func NewApp(cfg config.ServerConfig) *App {
 
 func (a *App) Run() error {
 
-	defer a.cleanup() // end with cleanup
+	defer a.cleanup()
 
-	// init components like input, network, state, etc..
 	if err := a.initComponents(); err != nil {
 		return fmt.Errorf("failed to initialize components: %w", err)
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go a.network.StartReceiving(ctx)
+
 	a.running = true
+	a.updateLoop()
 
 	return nil
 }
@@ -36,12 +46,40 @@ func (a *App) Run() error {
 func (a *App) initComponents() error {
 	var err error
 
-	a.network, err = NewNetwork(a.config.Port)
+	a.gameState = NewGameState()
+	a.clientManager = NewClientManager(a.gameState, a.config)
+	a.network, err = NewNetwork(a.config.Port, a.clientManager, a.gameState)
 	if err != nil {
 		return fmt.Errorf("network initialization failed: %w", err)
 	}
+	a.physicsEngine = NewPhysics(a.gameState, a.clientManager)
 
 	return nil
+}
+
+func (a *App) updateLoop() {
+	ticker := time.NewTicker(time.Second / time.Duration(a.config.PhysicsTickRate))
+	defer ticker.Stop()
+
+	updateCount := int64(0)
+	lastSend := 0
+	sendRatio := float64(a.config.NetworkSendRate) / float64(a.config.PhysicsTickRate)
+
+	for range ticker.C {
+		if !a.running {
+			break
+		}
+
+		updateCount++
+		a.network.SetUpdateCount(updateCount)
+
+		a.physicsEngine.Update()
+		a.clientManager.RemoveDisconnected(updateCount)
+		if (sendRatio*float64(updateCount))-float64(lastSend) >= 1 {
+			lastSend++
+			a.network.BroadcastGameState()
+		}
+	}
 }
 
 func (a *App) cleanup() {
