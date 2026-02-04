140c5c56c24a7abaec3244c7f62fed9583940d30
Tomek Petrykowski <tomasz.petrykowski.20@gmail.com>
2026-02-04 13:17:40 +0100

    Add player list to the scene

--- internal/client/scene_main_menu.go
+++ internal/client/scene_main_menu.go
@@ -1,17 +1,27 @@
 package client
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 type MainMenuScene struct {
-	app *App
+	app           *App
+	serverBrowser *ServerBrowser
+	refreshTimer  float32
 }
 
 func NewMainMenuScene(app *App) *MainMenuScene {
-	return &MainMenuScene{app: app}
+	return &MainMenuScene{
+		app:           app,
+		serverBrowser: NewServerBrowser(app.config.ServerIP, app.config.ServerPort),
+	}
 }
 
 func (s *MainMenuScene) OnEnter() {
 	rl.ShowCursor()
+	s.serverBrowser.Refresh()
 }
 
 func (s *MainMenuScene) OnExit() {
@@ -19,6 +29,13 @@ func (s *MainMenuScene) OnExit() {
 }
 
 func (s *MainMenuScene) Update() SceneType {
+	// refresh player list every 3 seconds
+	s.refreshTimer += rl.GetFrameTime()
+	if s.refreshTimer >= 3.0 {
+		s.refreshTimer = 0
+		s.serverBrowser.Refresh()
+	}
+
 	if rl.IsKeyPressed(rl.KeyEnter) {
 		return SceneGame
 	}
@@ -31,20 +48,65 @@ func (s *MainMenuScene) Update() SceneType {
 }
 
 func (s *MainMenuScene) Render() {
+	screenWidth := int32(s.app.config.WindowWidth)
+	screenHeight := int32(s.app.config.WindowHeight)
+	centerX := screenWidth / 2
+
 	rl.BeginDrawing()
-	rl.ClearBackground(rl.DarkGray)
+	rl.ClearBackground(rl.NewColor(30, 30, 40, 255))
 
-	title := "G-Game"
-	titleWidth := rl.MeasureText(title, 40)
-	rl.DrawText(title, (int32(s.app.config.WindowWidth)-titleWidth)/2, 150, 40, rl.White)
+	// Title
+	title := s.app.config.WindowTitle
+	titleWidth := rl.MeasureText(title, 60)
+	rl.DrawText(title, centerX-titleWidth/2, 100, 60, rl.White)
 
-	startText := "Press ENTER to Start"
-	startWidth := rl.MeasureText(startText, 20)
-	rl.DrawText(startText, (int32(s.app.config.WindowWidth)-startWidth)/2, 300, 20, rl.LightGray)
+	// Press ENTER to start
+	startText := "Press ENTER to start"
+	startWidth := rl.MeasureText(startText, 24)
+	rl.DrawText(startText, centerX-startWidth/2, 200, 24, rl.Green)
 
-	exitText := "Press ESC to Exit"
-	exitWidth := rl.MeasureText(exitText, 20)
-	rl.DrawText(exitText, (int32(s.app.config.WindowWidth)-exitWidth)/2, 340, 20, rl.LightGray)
+	// ESC to exit
+	exitText := "ESC - Exit"
+	exitWidth := rl.MeasureText(exitText, 16)
+	rl.DrawText(exitText, centerX-exitWidth/2, 240, 16, rl.Gray)
+
+	// Bottom: Server status and player list
+	bottomY := screenHeight - 120
+
+	// Server status line
+	var statusText string
+	var statusColor rl.Color
+	if s.serverBrowser.Online {
+		statusText = fmt.Sprintf("Server %s:%d - Online", s.app.config.ServerIP, s.app.config.ServerPort)
+		statusColor = rl.Green
+	} else {
+		statusText = fmt.Sprintf("Server %s:%d - Offline", s.app.config.ServerIP, s.app.config.ServerPort)
+		statusColor = rl.Red
+	}
+	rl.DrawText(statusText, 20, bottomY, 16, statusColor)
+
+	// Player list
+	if s.serverBrowser.Online {
+		playerCount := len(s.serverBrowser.Players)
+		if playerCount == 0 {
+			rl.DrawText("No players online", 20, bottomY+22, 14, rl.Gray)
+		} else {
+			playerText := fmt.Sprintf("Players online: %d", playerCount)
+			rl.DrawText(playerText, 20, bottomY+22, 14, rl.LightGray)
+
+			// List player IDs (compact)
+			x := int32(20)
+			for i, p := range s.serverBrowser.Players {
+				if i >= 8 {
+					rl.DrawText("...", x, bottomY+44, 14, rl.Gray)
+					break
+				}
+				idText := fmt.Sprintf("#%d", p.ID)
+				rl.DrawText(idText, x, bottomY+44, 14, rl.LightGray)
+				x += int32(rl.MeasureText(idText, 14)) + 15
+			}
+		}
+	}
 
 	rl.EndDrawing()
 }
